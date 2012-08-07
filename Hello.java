ff7ae3dc4d4f9b49162e32b2ea7e34a15f498fbb
lvail <lvail@olivet.edu>
2012-08-07 16:21:38 -0500

    Added header comments
    Signed-off-by: lvail <lvail@olivet.edu>

--- Hello.java
+++ Hello.java
@@ -1,8 +1,12 @@
+// Hello World Git project
+// by Larry Vail
+// August 7, 2012
+
 public class Hello {
 
 	public static void main(String [] args)
 	{
-		System.out.print("Hello");
+		System.out.print("Hello World (ldv)");
 	}
 
 }
